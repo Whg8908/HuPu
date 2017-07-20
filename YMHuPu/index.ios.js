484575c56b1d68ad443575616c54bcff8339557b
wow_chen <1042528249@qq.com>
2017-07-20 10:15:04 +0800

    开始更新

--- YMHuPu/index.ios.js
+++ YMHuPu/index.ios.js
@@ -17,7 +17,7 @@ export default class YMHuPu extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native!  开始RN
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
